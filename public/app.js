35b7699b901413d126b387692a985d6cbf20e46d
benpratama <ben021884@gmail.com>
2023-02-13 18:01:34 +0800

    day2 react

--- public/app.js
+++ public/app.js
@@ -1,82 +1,162 @@
 /**
-       * ini tempat buat ngerender
-      */
+ * ini tempat buat ngerender
+*/
 const root = document.querySelector('#root');
-console.log("ajinggg");
+
 /**
  * ini untuk buat elmentnya
 */
-const h1_1 = React.createElement('h1', {
-  children: 'kontolll ayam',
-  className: 'heading1'
-});
-const h1_2 = React.createElement('h1', {
-  children: 'kontolll ayam2',
-  className: 'heading2'
-});
-const list = /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "mantap"), /*#__PURE__*/React.createElement("li", null, "mantap2"), /*#__PURE__*/React.createElement("li", null, "mantap3"));
-var nama = 'asuu';
-const h1_3 = /*#__PURE__*/React.createElement("h1", {
-  class: nama
-}, "Tanggal sekarang", new Date().toLocaleDateString(), " nama: ", nama);
 
+// ======================================================
+// const h1_1 = React.createElement('h1',{
+//   children: 'kontolll ayam',
+//   className: 'heading1'
+// });
+
+// const h1_2 = React.createElement('h1',{
+//   children: 'kontolll ayam2',
+//   className: 'heading2'
+// });
+
+// const list = (
+//   <ul>
+//     <li>mantap</li>
+//     <li>mantap2</li>
+//     <li>mantap3</li>
+//   </ul>
+// )
+// var nama = 'asuu'
+// const h1_3 = <h1 class={nama}>Tanggal sekarang 
+//   {new Date().toLocaleDateString()} nama: {nama}</h1>
+
+// const element = React.createElement(React.Fragment,{
+//   children:[h1_3,list,h1_2,h1_1]
+// })
+// ReactDOM.render(element,root)
+
+// ======================================================
 /**
  * function yang returnnya jsx elment atau react element
  * itu componen
  * nama functionnya pake kapital depannya
  * parameternya yanh diterima itu bentuknya object
 */
-function Hallo(props) {
-  return /*#__PURE__*/React.createElement("p", null, "hallo ", /*#__PURE__*/React.createElement("b", null, props.name), " kamu kelas: ", /*#__PURE__*/React.createElement("b", null, props.kelas));
-}
-const par = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Hallo, {
-  name: "mantapp",
-  kelas: "12"
-}));
+// function Hallo (props) {
+//   return <p>hallo <b>{props.name}</b> kamu kelas: <b>{props.kelas}</b></p>
+// }
 
+// const par = (
+//   <>
+//   <Hallo name="mantapp" kelas="12"/>
+//   </>
+// )
+
+// ===================================================
 /**
  * function all ini isinya
  * 1. buat atribut time yang ambil waktu tiap detik
  * 2. create element
  * 3. render
+ * tujuannya buat interval
 */
-function all() {
-  const time = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
-    style: {
-      color: 'red'
-    }
-  }, "jam sekarang"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
+// function all(){
+//   const time = (
+//     <div>
+//       <h1 class="heading">jam sekarang</h1>
+//       <h2>{new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   )
 
-  /**
-   * kalo misalnya ada beberapa node, 
-   * jadi perlu di bungkus pake 1 elment (div),
-  */
-  // const element = React.createElement('div',{
-  //   children:[h1_1,h1_2]
-  // })
+//   /**
+//    * kalo misalnya ada beberapa node, 
+//    * jadi perlu di bungkus pake 1 elment (div),
+//   */
+//   // const element = React.createElement('div',{
+//   //   children:[h1_1,h1_2]
+//   // })
 
-  /**
-   * kalo gak mau dijadiin 1 element dia bisa dijadiin fragemnt 
-   * nanti childrennya isinya, list
-  */
-  const element = React.createElement(React.Fragment, {
-    children: [par, time]
-  });
+//   /**
+//    * kalo gak mau dijadiin 1 element dia bisa dijadiin fragemnt 
+//    * nanti childrennya isinya, list
+//   */
+//   const element = React.createElement(React.Fragment,{
+//     children:[par,time]
+//   })
 
-  /**
-   * ini buat render
-  */
+//   /**
+//    * ini buat render
+//   */
 
-  ReactDOM.render(element, root);
-}
+//   ReactDOM.render(element,root)
+// }
 
+// /**
+//  * ini buat manggil tiap detiknya
+// */
+// setInterval(function(){all()},1000)
+
+// ========================================
 /**
- * ini buat manggil tiap detiknya
-*/
-setInterval(function () {
-  all();
-}, 1000);
+ * event listener
+ */
+// function pasDiClick (msg){
+//   alert(msg)
+// }
+// const button1 = <button onClick={pasDiClick.bind(this,'mantappp bingit')}>click me</button>
+// const element = React.createElement(React.Fragment,{
+//   children:[button1]
+// })
+// function plus(count){
+//   return count+1;
+// }
 
+// function App(){
+//   // Destructuring
+//   const [count, setCount] = React.useState(1)
+//   // const count = state[0]
+//   // const updateCount = state[1]
+//   console.log(setCount)
+
+//   return(
+//     <>
+//       <button onClick={function(){setCount(count-1)}}>-</button>
+//       <span>{count}</span>
+//       <button onClick={function(){setCount(count+1)}} >+</button>
+//     </>
+//   )
+// }
+
+// ReactDOM.render(<App />,root)
+
+// =============================================
+function App() {
+  const [diklik, setDiklik] = React.useState(false);
+  const [count, setCOunt] = React.useState(1);
+
+  // ini bakal dieksekusi pas pertama kali dan adaperubahan
+  // parameter ke2 difokusin ->jadi kalo ada perubaha di valuenya dia bakal dieksekusi
+  // kalo arraynya kosong dia cuma dieksekusi pas pertama kali
+  // kalo gak pake array dia dirender terus
+  React.useEffect(function () {
+    console.log(document.getElementById('mantap'));
+  }, [diklik]);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    id: "mantap"
+  }, "mantap"), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setCOunt(count + 1);
+    }
+  }, "WUAAA"), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setDiklik(true);
+    }
+  }, "t/f"), /*#__PURE__*/React.createElement("span", null, diklik));
+}
+setTimeout(function () {
+  ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
+}, 100);
+
+// =============================================
 /**
 //  ini cara pure js
 // AMBIL ROOTNYA DULU
--- src/app.js
+++ src/app.js
@@ -1,91 +1,167 @@
 /**
-       * ini tempat buat ngerender
-      */
+ * ini tempat buat ngerender
+*/
 const root = document.querySelector('#root')
-console.log("ajinggg")
+
 /**
  * ini untuk buat elmentnya
 */
-const h1_1 = React.createElement('h1',{
-  children: 'kontolll ayam',
-  className: 'heading1'
-});
 
-const h1_2 = React.createElement('h1',{
-  children: 'kontolll ayam2',
-  className: 'heading2'
-});
+// ======================================================
+// const h1_1 = React.createElement('h1',{
+//   children: 'kontolll ayam',
+//   className: 'heading1'
+// });
 
-const list = (
-  <ul>
-    <li>mantap</li>
-    <li>mantap2</li>
-    <li>mantap3</li>
-  </ul>
-)
-var nama = 'asuu'
-const h1_3 = <h1 class={nama}>Tanggal sekarang 
-  {new Date().toLocaleDateString()} nama: {nama}</h1>
+// const h1_2 = React.createElement('h1',{
+//   children: 'kontolll ayam2',
+//   className: 'heading2'
+// });
 
+// const list = (
+//   <ul>
+//     <li>mantap</li>
+//     <li>mantap2</li>
+//     <li>mantap3</li>
+//   </ul>
+// )
+// var nama = 'asuu'
+// const h1_3 = <h1 class={nama}>Tanggal sekarang 
+//   {new Date().toLocaleDateString()} nama: {nama}</h1>
+
+// const element = React.createElement(React.Fragment,{
+//   children:[h1_3,list,h1_2,h1_1]
+// })
+// ReactDOM.render(element,root)
+
+
+// ======================================================
 /**
  * function yang returnnya jsx elment atau react element
  * itu componen
  * nama functionnya pake kapital depannya
  * parameternya yanh diterima itu bentuknya object
 */
-function Hallo (props) {
-  return <p>hallo <b>{props.name}</b> kamu kelas: <b>{props.kelas}</b></p>
-}
+// function Hallo (props) {
+//   return <p>hallo <b>{props.name}</b> kamu kelas: <b>{props.kelas}</b></p>
+// }
 
-const par = (
-  <>
-  <Hallo name="mantapp" kelas="12"/>
-  </>
-)
+// const par = (
+//   <>
+//   <Hallo name="mantapp" kelas="12"/>
+//   </>
+// )
 
+
+// ===================================================
 /**
  * function all ini isinya
  * 1. buat atribut time yang ambil waktu tiap detik
  * 2. create element
  * 3. render
+ * tujuannya buat interval
 */
-function all(){
-  const time = (
-    <div>
-      <h1 style={{color:'red'}}>jam sekarang</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
+// function all(){
+//   const time = (
+//     <div>
+//       <h1 class="heading">jam sekarang</h1>
+//       <h2>{new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   )
 
-  /**
-   * kalo misalnya ada beberapa node, 
-   * jadi perlu di bungkus pake 1 elment (div),
-  */
-  // const element = React.createElement('div',{
-  //   children:[h1_1,h1_2]
-  // })
+//   /**
+//    * kalo misalnya ada beberapa node, 
+//    * jadi perlu di bungkus pake 1 elment (div),
+//   */
+//   // const element = React.createElement('div',{
+//   //   children:[h1_1,h1_2]
+//   // })
 
-  /**
-   * kalo gak mau dijadiin 1 element dia bisa dijadiin fragemnt 
-   * nanti childrennya isinya, list
-  */
-  const element = React.createElement(React.Fragment,{
-    children:[par,time]
-  })
+//   /**
+//    * kalo gak mau dijadiin 1 element dia bisa dijadiin fragemnt 
+//    * nanti childrennya isinya, list
+//   */
+//   const element = React.createElement(React.Fragment,{
+//     children:[par,time]
+//   })
   
-  /**
-   * ini buat render
-  */
+//   /**
+//    * ini buat render
+//   */
 
-  ReactDOM.render(element,root)
-}
+//   ReactDOM.render(element,root)
+// }
 
+// /**
+//  * ini buat manggil tiap detiknya
+// */
+// setInterval(function(){all()},1000)
+
+
+// ========================================
 /**
- * ini buat manggil tiap detiknya
-*/
-setInterval(function(){all()},1000)
+ * event listener
+ */
+// function pasDiClick (msg){
+//   alert(msg)
+// }
+// const button1 = <button onClick={pasDiClick.bind(this,'mantappp bingit')}>click me</button>
+// const element = React.createElement(React.Fragment,{
+//   children:[button1]
+// })
+// function plus(count){
+//   return count+1;
+// }
+
+// function App(){
+//   // Destructuring
+//   const [count, setCount] = React.useState(1)
+//   // const count = state[0]
+//   // const updateCount = state[1]
+//   console.log(setCount)
+
+//   return(
+//     <>
+//       <button onClick={function(){setCount(count-1)}}>-</button>
+//       <span>{count}</span>
+//       <button onClick={function(){setCount(count+1)}} >+</button>
+//     </>
+//   )
+// }
 
 
+// ReactDOM.render(<App />,root)
+
+// =============================================
+function App(){
+  const [diklik,setDiklik] = React.useState(false)
+  const [count,setCOunt] = React.useState(1)
+  
+  // ini bakal dieksekusi pas pertama kali dan adaperubahan
+  // parameter ke2 difokusin ->jadi kalo ada perubaha di valuenya dia bakal dieksekusi
+  // kalo arraynya kosong dia cuma dieksekusi pas pertama kali
+  // kalo gak pake array dia dirender terus
+  React.useEffect(function(){
+    console.log(document.getElementById('mantap'),)
+  },[diklik])
+
+  return(
+    <>
+      <h1 id="mantap">mantap</h1>
+      <button onClick={function(){setCOunt(count+1)}}>WUAAA</button>
+      <button onClick={function(){setDiklik(true)}}>t/f</button>
+      <span>{diklik}</span>
+    </>
+
+  )
+}
+setTimeout(function(){
+  ReactDOM.render(<App />,root)
+},100)
+
+
+
+// =============================================
 /**
 //  ini cara pure js
 // AMBIL ROOTNYA DULU
